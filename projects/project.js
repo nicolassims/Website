6b3728d66567e2b271c7273d36d1d4bd6c0c4ced
nicolassims <sableye.nico@gmail.com>
2021-06-14 17:04:10 -0700

    Working on fleshing out gallery code.

--- projects/project.js
+++ projects/project.js
@@ -5,6 +5,23 @@ if (!theme) {//if the user hasn't set a theme before...
     setTheme(theme);//set their previously-selected theme
 }
 
+var imagelist = document.getElementsByClassName("gallery-image");
+for (var i = 0; i < imagelist.length; i++) {
+    imagelist[i].addEventListener('click', function () {
+        if (!this.classList.contains("enlarged")) {
+            shrinkAll();
+            this.classList.add("enlarged");
+        } else {
+            shrinkAll();
+        }
+    })
+}
+
+setInterval(function () {
+    var ele = document.getElementById('blinker');
+    ele.style.visibility = (ele.style.visibility == 'hidden' ? '' : 'hidden');
+}, 1000);
+
 function setTheme(mode) {
     if (mode == 'light') {//if the mode was light...
         document.getElementById('theme-style').href = '../default.css';//then set the css style to the default
@@ -13,7 +30,9 @@ function setTheme(mode) {
     }
 }
 
-setInterval(function () {
-    var ele = document.getElementById('blinker');
-    ele.style.visibility = (ele.style.visibility == 'hidden' ? '' : 'hidden');
-}, 1000);
+function shrinkAll() {
+    imagelist = document.getElementsByClassName("gallery-image");
+    for (var i = 0; i < imagelist.length; i++) {
+        imagelist[i].classList.remove("enlarged");
+    }
+}
