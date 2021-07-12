58cac0238dfb0b3b8b25315267196011b55f9779
nicolassims <sableye.nico@gmail.com>
2021-07-12 16:56:44 -0500

    Now utilizing modal image zoom.

--- projects/project.js
+++ projects/project.js
@@ -5,18 +5,6 @@ if (!theme) {//if the user hasn't set a theme before...
     setTheme(theme);//set their previously-selected theme
 }
 
-var imagelist = document.getElementsByClassName("gallery-image");
-for (var i = 0; i < imagelist.length; i++) {
-    imagelist[i].addEventListener('click', function () {
-        if (!this.classList.contains("enlarged")) {
-            shrinkAll();
-            this.classList.add("enlarged");
-        } else {
-            shrinkAll();
-        }
-    })
-}
-
 let pw = document.querySelector('.gallery-wrapper');//get the gallery-wrapper element, which is the parent of all the posts...
 for (var i = pw.children.length; i >= 0; i--) {//look through every child in the post-wrapper, then...
     pw.appendChild(pw.children[Math.random() * i | 0]);//append that child in a new order.
@@ -35,9 +23,18 @@ function setTheme(mode) {
     }
 }
 
-function shrinkAll() {
-    imagelist = document.getElementsByClassName("gallery-image");
-    for (var i = 0; i < imagelist.length; i++) {
-        imagelist[i].classList.remove("enlarged");
-    }
+var imagelist = document.getElementsByClassName("gallery-image");
+for (var i = 0; i < imagelist.length; i++) {
+    imagelist[i].addEventListener('click', function () {
+        var modal = document.getElementById("myModal");
+        var modalImg = document.getElementById("modal-container");
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        modalImg.title = this.title;
+    })
 }
+
+var modal = document.getElementById("myModal");
+modal.addEventListener('click', function () {
+    modal.style.display = "none";
+})
